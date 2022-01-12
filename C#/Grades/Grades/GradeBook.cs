c286050473e7a630168a91722966cb1ff9b6ac1a
Whirl84 <45568202+Whirl84@users.noreply.github.com>
2022-01-12 09:28:30 +0000

    Add files via upload

---
+++ C#/Grades/Grades/GradeBook.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    class GradeBook
+    {
+        public static float MinimumGrade = 0;
+        public static float MaximumGrade = 100;
+        private string _name;
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if(_name != value)
+                {
+                    var oldValue = _name;
+                    _name = value;
+                    if (NameChanged != null)
+                    {
+                        NameChangedEventArgs args = new NameChangedEventArgs();
+                        args.OldValue = oldValue;
+                        args.NewValue = value;
+                        NameChanged(this, args);
+                    }
+                }
+            }
+        }
+
+        public event NameChangedDelegate NameChanged;
+
+        public GradeBook(string name = "There is no name")
+        {
+            grades = new List<float>();
+            Name = name;
+        }
+
+        public void AddGrade(float grade)
+        {
+            if (grade >= 0 && grade <= 100)
+            {
+                grades.Add(grade);
+            }
+        }
+
+        public GradeStatistics ComputeStatistics()
+        {
+            GradeStatistics stats = new GradeStatistics();
+            
+            float sum = 0f;
+            foreach(float grade in grades)
+            {
+                stats.HighestGrade = Math.Max(grade, stats.HighestGrade);
+                stats.LowestGrade  = Math.Min(grade, stats.LowestGrade);
+                sum += grade;
+            }
+            stats.AverageGrade = sum / grades.Count;
+
+            return stats;
+        }
+
+        private List<float> grades;
+    }
+}
