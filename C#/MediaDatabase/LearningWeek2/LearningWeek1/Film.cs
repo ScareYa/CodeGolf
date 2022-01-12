10cd4d8e40f1cf79a0a3eef02757124b69b73193
Whirl84 <45568202+Whirl84@users.noreply.github.com>
2022-01-12 09:34:44 +0000

    Add files via upload

---
+++ C#/MediaDatabase/LearningWeek2/LearningWeek1/Film.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningWeek1
+{
+    class Film
+    {
+        private string _title = "";
+        private Int16 _rating = 0;
+
+        public Film(string Title, Int16 Rating)
+        {
+            //Films = new List<string>();
+            _title  = Title;
+            _rating = Rating;
+        }
+
+        public string GetTitle()                 { return _title;       }
+        public Int16  GetRating()                { return _rating;      }
+        public void   SetTitle(string NewTitle)  { _title = NewTitle;   }
+        public void   SetRating(Int16 NewRating) { _rating = NewRating; }
+
+        //private List<string> Films;
+    }
+}
---
+++ C#/MediaDatabase/LearningWeek2/LearningWeek1/FilmStatistics.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningWeek1
+{
+    class FilmStatistics
+    {
+        public Int16 maxRating = 10;
+        public Int16 minRating = 1;
+        public float avgRating = 0f;
+
+        public FilmStatistics()
+        {
+
+        }
+    }
+}
